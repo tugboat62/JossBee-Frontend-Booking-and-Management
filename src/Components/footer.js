700fe1d96d25566408a613bdc3bf1f79ccd502fb
tugboat62 <mujadded1998@gmail.com>
2023-08-20 01:50:28 +0600

    footer social media icons added

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import '../Styles/footer.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../Styles/footer.css";
+import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import { Facebook, Twitter, Instagram } from "@mui/icons-material";
 const Footer = () => {
   return (
     <footer className="footer">
@@ -8,19 +10,31 @@ const Footer = () => {
         <div className="footer-content">
           <div className="footer-links">
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/explore">Explore</Link></li>
-              <li><Link to="/about">About Us</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/explore">Explore</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
             </ul>
           </div>
-          {/* <div className="footer-social">
-            <tr>
-              <td><Link to="/"><i className="fab fa-facebook"></i></Link></td>
-              <td><Link to="/"><i className="fab fa-twitter"></i></Link></td>
-              <td><Link to="/"><i className="fab fa-instagram"></i></Link></td>
-            </tr>
-          </div> */}
+          <div className="social-icons">
+            <IconButton aria-label="Facebook">
+              <Facebook />
+            </IconButton>
+            <IconButton aria-label="Twitter">
+              <Twitter />
+            </IconButton>
+            <IconButton aria-label="Instagram">
+              <Instagram />
+            </IconButton>
+          </div>
         </div>
         <div className="footer-bottom">
           <p>&copy; 2023 JossBee. All rights reserved.</p>
